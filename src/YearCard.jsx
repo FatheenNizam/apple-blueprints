3be5edf5d2dbfe8d644bf7b0a7e87269624f8957
Fatheen Nizam <fatheen.nizam@icloud.com>
2024-07-06 02:00:35 -0400

    Add ability to automatically jump to current year on page load

--- src/YearCard.jsx
+++ src/YearCard.jsx
@@ -1,13 +1,75 @@
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { MonthCard } from "./MonthCard";
 
 export function YearCard({ months, year }) {
   const currentYear = new Date().getFullYear();
-  const isPastYear = year < currentYear; // Check if year is less than currentYear
+  const isPastYear = year < currentYear;
+
+  const yearLabelRef = useRef(null);
+  const [isScrollingProgrammatically, setIsScrollingProgrammatically] = useState(false);
+
+  useEffect(() => {
+    let lastTop = 0;
+    let lastScrollPosition = 0;
+    const navbar = document.querySelector("#navbar");
+    const navbarTravel = navbar.clientHeight + 24;
+
+    // Set navbar to visible initially
+    navbar.style.transform = `translateY(0)`;
+
+    const handleScroll = () => {
+      if (!isScrollingProgrammatically) {
+        const maxScrollPosition =
+          Math.max(
+            document.body.scrollHeight,
+            document.body.offsetHeight,
+            document.documentElement.clientHeight,
+            document.documentElement.scrollHeight,
+            document.documentElement.offsetHeight
+          ) - window.innerHeight;
+
+        const scrollPosition = window.pageYOffset;
+        const delta = scrollPosition - lastScrollPosition;
+        const newTop = scrollPosition <= 0 ? 0 : Math.max(0, Math.min(navbarTravel, lastTop + delta));
+
+        navbar.style.transform = `translateY(-${newTop}px)`;
+
+        lastTop = newTop;
+        lastScrollPosition = scrollPosition;
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, [isScrollingProgrammatically]);
+
+  useEffect(() => {
+    if (year === currentYear) {
+      const offset = -140; // Adjust this value as needed
+      const element = yearLabelRef.current;
+      const elementPosition = element.getBoundingClientRect().top + window.pageYOffset;
+
+      setIsScrollingProgrammatically(true);
+      window.scrollTo({
+        top: elementPosition + offset,
+        behavior: "auto", // Ensure no scrolling animation
+      });
+
+      // Disable the programmatic scroll flag after a short delay to avoid conflicts
+      setTimeout(() => {
+        setIsScrollingProgrammatically(false);
+      }, 100); // Adjust the delay as needed
+    }
+  }, [currentYear, year]);
 
   return (
     <div className="yearCard">
-      <h2 className={`year-label ${isPastYear ? "past-date" : ""}`}>{year}</h2>
+      <h2 ref={yearLabelRef} className={`year-label ${isPastYear ? "past-date" : ""}`}>
+        {year}
+      </h2>
       <div className="container">
         {months.map(({ name, products }) => (
           <MonthCard
