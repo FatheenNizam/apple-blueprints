bf841662e2359835fd725da97ea528c541a9d4bb
Fatheen Nizam <fatheen.nizam@icloud.com>
2024-07-13 02:02:41 -0400

    Update something

--- src/fetchSiteContentData.js
+++ src/fetchSiteContentData.js
@@ -1,7 +1,7 @@
 export async function fetchSiteContentData() {
   const spaceId = import.meta.env.VITE_CONTENTFUL_SPACE_ID;
   const environmentId = import.meta.env.VITE_CONTENTFUL_ENVIRONMENT_ID;
-  const entryId = "4NbYhPPGmnIHBtE5KBp3oz";
+  const entryId = import.meta.env.VITE_CONTENTFUL_ENTRY_ID;
   const accessToken = import.meta.env.VITE_CONTENTFUL_ACCESS_TOKEN;
   const apiUrl = `https://cdn.contentful.com/spaces/${spaceId}/environments/${environmentId}/entries/${entryId}?access_token=${accessToken}`;
   const data = await fetch(apiUrl).then((res) => res.json());
