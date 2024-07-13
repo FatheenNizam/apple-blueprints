6ec5213366f128c177cb404a1408770a65419761
Fatheen Nizam <fatheen.nizam@icloud.com>
2024-07-13 00:55:09 -0400

    Highlight current month

--- src/MonthCard.jsx
+++ src/MonthCard.jsx
@@ -9,21 +9,24 @@ export function MonthCard({ month, products, year, isPastYear }) {
 
   const currentYear = new Date().getFullYear();
   const today = new Date();
-  const currentMonth = today.getMonth() + 1; // getMonth returns 0-based index
-
-  // Parse the month string to a Date object with the current year
+  const currentMonth = today.getMonth() + 1;
   const monthDate = parse(`${month} ${year}`, "MMMM yyyy", new Date());
 
-  // Calculate start and end of current month
   const startCurrentMonth = startOfMonth(today);
   const endCurrentMonth = endOfMonth(today);
 
-  // Check if the monthDate is before the start of current month and year <= currentYear
   const isPastMonth = isBefore(monthDate, startCurrentMonth) && year <= currentYear;
+  const isCurrentMonth = monthDate.getFullYear() === currentYear && monthDate.getMonth() + 1 === currentMonth;
 
   return (
     <ul className="month-card">
-      <h3 className={`month-label ${isPastYear || isPastMonth ? "past-date" : ""}`}>{month}</h3>
+      <h3
+        className={`month-label ${isPastYear || isPastMonth ? "past-date" : ""} ${
+          isCurrentMonth ? "current-month" : ""
+        }`}
+      >
+        {month}
+      </h3>
       <div className="product-list-container">
         {products.map((product) => (
           <ProductItem key={product.sys.id} product={product} />
