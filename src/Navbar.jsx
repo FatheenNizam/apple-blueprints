64fb0e25c2808b9cafaf3a0ea642e2e95356e764
Fatheen Nizam <fatheen.nizam@icloud.com>
2024-08-06 14:09:57 -0400

    Add mobile dropdown menu

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,39 +1,69 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import { SiteContentContext } from "./SiteContentContext";
 
 export function Navbar() {
   const siteContentData = useContext(SiteContentContext);
+  const [isDropdownVisible, setIsDropdownVisible] = useState(false);
+
+  const toggleDropdown = (e) => {
+    e.preventDefault();
+    setIsDropdownVisible(!isDropdownVisible);
+  };
 
   return (
-    <nav id="navbar" className="section" role="navigation" aria-label="Main navigation">
-      <h1>
-        <a
-          id="site-title"
-          href="#top"
-          aria-current="page"
-          aria-label="Jump to top of the page"
-          title="Scroll to the top of the page"
-        >
-          {siteContentData?.fields.siteTitle}
-        </a>
-      </h1>
-      <div className="navbar-menu-items">
-        {siteContentData?.fields.menuItems?.map((item) => {
-          const { fields: { title, url } = {} } = item || {};
+    <>
+      <nav id="navbar" className="section" role="navigation" aria-label="Main navigation">
+        <h1>
+          <a
+            id="site-title"
+            href="#top"
+            aria-current="page"
+            aria-label="Jump to top of the page"
+            title="Scroll to the top of the page"
+          >
+            {siteContentData?.fields.siteTitle}
+          </a>
+        </h1>
+        <div className="menu-items-container">
+          {siteContentData?.fields.menuItems?.map((item) => {
+            const { fields: { title, url } = {} } = item || {};
 
-          return title ? (
-            <a className="desktop-item menu-item" key={title} href={url || "#"} aria-label={title}>
-              {title}
-            </a>
-          ) : null;
-        })}
-        <a id="search-icon" className="desktop-item mobile-item menu-item" href="#" aria-label="Search">
-          <i className="fa-solid fa-magnifying-glass"></i>
-        </a>
-        <a id="mobile-menu-icon" className="mobile-item menu-item" href="#" aria-label="Menu">
-          <i class="fa-solid fa-bars"></i>
-        </a>
-      </div>
-    </nav>
+            return title ? (
+              <a className="menu-item" key={title} href={url || "#"} aria-label={title}>
+                {title}
+              </a>
+            ) : null;
+          })}
+          <a id="search-icon" className="menu-item" href="#" aria-label="Search">
+            <i className="fa-solid fa-magnifying-glass"></i>
+          </a>
+          <a id="mobile-menu-icon" className="menu-item" href="#" aria-label="Menu" onClick={toggleDropdown}>
+            <i className={`fa-solid ${isDropdownVisible ? "fa-close" : "fa-bars"}`}></i>
+          </a>
+        </div>
+      </nav>
+      {isDropdownVisible && (
+        <div id="mobile-dropdown-menu" className="mobile-dropdown">
+          <a
+            id="mobile-dropdown-close-button"
+            className="mobile-dropdown-menu-item"
+            href="#"
+            aria-label="Search"
+            onClick={toggleDropdown}
+          >
+            <i className="fa-solid fa-close"></i>
+          </a>
+          {siteContentData?.fields.menuItems?.map((item) => {
+            const { fields: { title, url } = {} } = item || {};
+
+            return title ? (
+              <a className="mobile-dropdown-menu-item" key={title} href={url || "#"} aria-label={title}>
+                {title}
+              </a>
+            ) : null;
+          })}
+        </div>
+      )}
+    </>
   );
 }
