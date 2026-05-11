317eb66c781480fd6bd8668963372d68b5f1bf02
Fatheen Nizam <fatheen.nizam@icloud.com>
2026-05-10 22:21:25 -0400

    Potential fix to missing products

--- src/fetchProductsData.js
+++ src/fetchProductsData.js
@@ -1,3 +1,7 @@
 import { contentfulClient } from "./contentfulClient";
 
-export const fetchProductsData = async () => await contentfulClient.getEntries({ content_type: "product" });
+export const fetchProductsData = async () =>
+  await contentfulClient.getEntries({
+    content_type: "product",
+    limit: 1000,
+  });
